d194d1bd5a1eeba42c190d1a264da05e04007be9
RushikeshJoshi4 <rushikeshjoshi41097@gmail.com>
2019-10-09 13:22:41 -0700

    Initializing

--- scripts/py1_testing.py
+++
@@ -1,9 +0,0 @@
-import pygame
-import sys
-import time
-pygame.init()
-screen = pygame.display.set_mode((1000,100))
-
-while True:
-    for event in pygame.event.get():
-        print("time: "+str(time.time()))
